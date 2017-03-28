88b8c468d80b4c51f26150417951c99c9ecbdb0a
Eliecer <ramirez.esquinca@gmail.com>
2017-03-28 08:53:24 -0600

    Manual tecnico docs

---
+++ docs/js/docs-setup.js
@@ -0,0 +1,249 @@
+NG_DOCS={
+  "sections": {
+    "api": "Documentación"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "App.Auth",
+      "shortName": "App.Auth",
+      "type": "service",
+      "moduleName": "App",
+      "shortDescription": "Proporciona los metodos para la autenticacion y permisos, estos pueden ser invocados desde el controlador.",
+      "keywords": "api app autenticacion autentificaci cerrar con controlador correcta credenciales data de del desde ejecutar el eliminar en error errorcallback es estos funcion invocados la las lo localstorage logout los mecanismo method metodos nuevo object obtiene para permisos prepara proporciona pueden refreshtoken respuesta se ser service sesi session si signin solicitar success successcallback tenga todo token usuario"
+    },
+    {
+      "section": "api",
+      "id": "App.AuthService",
+      "shortName": "App.AuthService",
+      "type": "service",
+      "moduleName": "App",
+      "shortDescription": "Proporciona los metodos para la autenticacion y permisos.",
+      "keywords": "activa api app autenticacion authservice comprueba con contiene contruir credenciales cuenta data datos de del el este getperfil getpermisos la las los menu method metodos modificar object obtener para parametros perfil permisos post proporciona reliza service tiene usuario validar"
+    },
+    {
+      "section": "api",
+      "id": "App.directive:errorFlash",
+      "shortName": "errorFlash",
+      "type": "directive",
+      "moduleName": "App",
+      "shortDescription": "Muestra en pantalla los mensajes que devuelve la api.",
+      "keywords": "api app caso con danger de del devuelve directive el en error errorflash flash info la los mensaje mensajes muestra object objeto pantalla ser success texto tipo warning"
+    },
+    {
+      "section": "api",
+      "id": "App.directive:focusMe",
+      "shortName": "focusMe",
+      "type": "directive",
+      "moduleName": "App",
+      "shortDescription": "Pone el foco en un elemento.",
+      "keywords": "api app directive el elemento en foco pone"
+    },
+    {
+      "section": "api",
+      "id": "App.directive:imprimirDiv",
+      "shortName": "imprimirDiv",
+      "type": "directive",
+      "moduleName": "App",
+      "shortDescription": "Genera un boton para imprimir el contenido de un area determinada en la vista.",
+      "keywords": "api app area boton clase class contenga contenido de determinada directive el en es genera html imprimir imprimir-div imprimirdiv la module para vista"
+    },
+    {
+      "section": "api",
+      "id": "App.directive:urlModulo",
+      "shortName": "urlModulo",
+      "type": "directive",
+      "moduleName": "App",
+      "shortDescription": "Genera la url (modificar, nuevo, ver, listar) de las vistas, sin necesidad de poner la ruta completa, solamente al metodo acceder.",
+      "keywords": "acceder al api app completa dato de directive genera html la las listar metodo modificar module necesidad nuevo poner ruta sin solamente url url-modulo urlmodulo ver vistas"
+    },
+    {
+      "section": "api",
+      "id": "App.filter:numberFixedLen",
+      "shortName": "numberFixedLen",
+      "type": "filter",
+      "moduleName": "App",
+      "shortDescription": "Fomato a numeros con 2 decimales.",
+      "keywords": "api app con decimales filter fomato numeros"
+    },
+    {
+      "section": "api",
+      "id": "App.interface:MENU",
+      "shortName": "MENU",
+      "type": "interface",
+      "moduleName": "App",
+      "shortDescription": "Contiene la configuración del menu del proyecto para usuarios logueados.",
+      "keywords": "api app configuraci contiene del interface la logueados menu para proyecto usuarios"
+    },
+    {
+      "section": "api",
+      "id": "App.interface:MENU_PUBLICO",
+      "shortName": "MENU_PUBLICO",
+      "type": "interface",
+      "moduleName": "App",
+      "shortDescription": "Contiene la configuración del menu del proyecto para la presentacion del módulo.",
+      "keywords": "api app configuraci contiene del interface la menu para presentacion proyecto"
+    },
+    {
+      "section": "api",
+      "id": "App.interface:Scaffolding",
+      "shortName": "Scaffolding",
+      "type": "interface",
+      "moduleName": "App",
+      "shortDescription": "A continuación se lista las carpetas y se explica el contenido de cada uno.",
+      "keywords": "acci agilizar angular api aplicaci app area areas bower_componets cada carga carpeta carpetas caso ccaiones codigo como contenido contiene continuaci controlador controladores crdu criterio dashboard datos de del dentro dialog el en encuentra especifica especificas estan estilos evaluaciones explica externamente extras filtrado genera general generan gr grafico importante indicador interface interior la las lista llamada lo los manejan manejo mas modulo modulos multiples necesita necesiten opciones otra otro para parte por principal programaci proporciona proyecto realizar recoleccion recurso se separa si son su sus svg terceros tipo todas todos una uno usen views vista vistas"
+    },
+    {
+      "section": "api",
+      "id": "App.interface:URLS",
+      "shortName": "URLS",
+      "type": "interface",
+      "moduleName": "App",
+      "shortDescription": "constantes para las urls utilizadas en la aplicación.",
+      "keywords": "api aplicaci app constantes en interface la las para urls utilizadas"
+    },
+    {
+      "section": "api",
+      "id": "App.Menu",
+      "shortName": "App.Menu",
+      "type": "service",
+      "moduleName": "App",
+      "shortDescription": "Crea el menu lateral izquierda según permisos.",
+      "keywords": "api app crea el izquierda lateral menu permisos seg service"
+    },
+    {
+      "section": "api",
+      "id": "App.service:config",
+      "shortName": "config",
+      "type": "service",
+      "moduleName": "App",
+      "shortDescription": "Contiene la configuración general del proyecto, precarga los iconos y el tema de material desing, ademas crea las rutas publicas.",
+      "keywords": "ademas api app configuraci contiene crea de del desing el general iconos la las los material precarga proyecto publicas rutas service tema"
+    },
+    {
+      "section": "api",
+      "id": "App.SigninCtrl",
+      "shortName": "App.SigninCtrl",
+      "type": "object",
+      "moduleName": "App",
+      "shortDescription": "Encargado del acceso al sistema.",
+      "keywords": "abre acceso al api apliaci app cierra dashboard del dispositivo el elimina en encargado hacer idioma ir la lateral local logout menu method metodo mostraridiomas muestra object obtiene pagina para peticiones redirecciona redireccionar se seleccion session si signin signinctrl sistema solicitada storage successauth togglemenu token"
+    },
+    {
+      "section": "api",
+      "id": "Catalogos.CriterioCtrl",
+      "shortName": "Catalogos.CriterioCtrl",
+      "type": "object",
+      "moduleName": "Catalogos",
+      "shortDescription": "Complemento del controlador CrudCtrl  para tareas especificas en criterios",
+      "keywords": "al api arbol area bandera cada carga cargarcatalogo cat catalago catalogo catalogos checkbox click colapsar como complemento con cone contiene contienen controlador crear criterioctrl criterios crudctrl data datos de defaul del determinar donde el en enviados especificas esta evento exista expandir formatea genero getindicadores hacer hijos indicador indicadores insertado interpretarla json la lo los lugar marca menos method nivel nombre object para pertenezca petici pinta poder por pueda se segun seleccionado ser si tareas tiene treeclick treeclickcheck uno url validar validarindicador valizaci valores verificacion ya"
+    },
+    {
+      "section": "api",
+      "id": "Catalogos.CrudCtrl",
+      "shortName": "Catalogos.CrudCtrl",
+      "type": "object",
+      "moduleName": "Catalogos",
+      "shortDescription": "Controlador general que maneja el CRUD(crear,leer,actualizar,eliminar) de la pagina",
+      "keywords": "$index actual actualiza actualizar agrega agregarclues antes api autocomplete ayuda bandera barra base borrar borrarclues boton buscar buscarl busqueda cambiar cambiartipo campo carga cargarcatalogo caso cat catalago catalogo catalogos clickeada clues columna como con confirmacion contiene control controlador correspondientes crear crud crudctrl cruddataapi cuando datos de del donde ejecute el elemento elimina eliminar en envia especificado especificados este evento existe filtrado form formulario general getclues grid guardar hacer href identificador identificar incia incializar inicializa init item jurisdicci jurisdiccion la lanzo las le leer limit limpiar lista listado llenado los maneja mensaje method modificar moodelo mostrarsearch muestra necesarios nombre nuevo object objeto obtiene oculta onorderchange ordenamiento order pagina paginado para parametro parametros parte pase petici por posicion procesar query querysearch quita quitar reciba recurso redirecciona registro regresar resultados ruta rutas se segun selecciona selecteditemchange servicio si texto tipo una url usuario valor ver vez vista vistas"
+    },
+    {
+      "section": "api",
+      "id": "Catalogos.IndicadorCtrl",
+      "shortName": "Catalogos.IndicadorCtrl",
+      "type": "object",
+      "moduleName": "Catalogos",
+      "shortDescription": "Complemento del controlador CrudCtrl  para tareas especificas en Indicador",
+      "keywords": "$index agrega agregaralerta al alerta alertas api buscar calcularminimo calculo catalogos complemento controlador crea crudctrl de del el elemento en especificas existe formulario getindexof hacer indicador indicadorctrl la las linea lista listado mas method minima object obtiene para posicion quitar quitaralerta si siguiente tareas ultimo una val valida valor"
+    },
+    {
+      "section": "api",
+      "id": "Catalogos.interface:CrudModule",
+      "shortName": "CrudModule",
+      "type": "interface",
+      "moduleName": "Catalogos",
+      "shortDescription": "Contiene la configuración de las rutas para los catalogos.",
+      "keywords": "api catalogos configuraci contiene de interface la las los para rutas"
+    },
+    {
+      "section": "api",
+      "id": "Catalogos.service:CrudDataApi",
+      "shortName": "CrudDataApi",
+      "type": "service",
+      "moduleName": "Catalogos",
+      "shortDescription": "Contiene los metodos para las peticiones a la api restfull.",
+      "keywords": "actualizar al api caso catalogos con contiene crear cruddataapi data dato datos de del direccion editar el elimina eliminar en envia error especificado especifico fallos function hacer identificador informaci insertar json la las lista los method metodos obtiene para peticion peticiones recuperar recurso recursos restfull resultados service success url ver"
+    },
+    {
+      "section": "api",
+      "id": "Dashboard.DashboardCtrl",
+      "shortName": "Dashboard.DashboardCtrl",
+      "type": "object",
+      "moduleName": "Dashboard",
+      "shortDescription": "Manejo de los eventos del gráfico en el dashboard",
+      "keywords": "accion activos agrega agrupado almacenar api aplicarfiltro array avanzado base bimestre buildtoggler cambiaranio cambiarbimestre cambiarvertodoindicador cambiarvertodoum cargar categoria change clues compprueba compsolo comrpueba con crea crear dashboard dashboardctrl dato datorueba datos de del el en es evento eventos exista exists extraer filtrado filtro fullscreen getdimension gr hacer identificador indicadores informaci insertar item la las list lista los manejo method modelo modo mostrar navid necesarios nivel object obtiene ocultar opciones para parametros poner por posicion procesar quitar quitarfiltro si sidenav tiene tipo toggle valor"
+    },
+    {
+      "section": "api",
+      "id": "Sistema.GrupoCtrl",
+      "shortName": "Sistema.GrupoCtrl",
+      "type": "object",
+      "moduleName": "Sistema",
+      "shortDescription": "Complemento del controlador CrudCtrl  para tareas especificas en Grupo",
+      "keywords": "api complemento controlador crear crudctrl datos del en especificas grupo grupoctrl grupover la los method moduloaccion object obtiene para permisos por sistema tareas tiene ver vista"
+    },
+    {
+      "section": "api",
+      "id": "Sistema.ModuloCtrl",
+      "shortName": "Sistema.ModuloCtrl",
+      "type": "object",
+      "moduleName": "Sistema",
+      "shortDescription": "Complemento del controlador CrudCtrl  para tareas especificas en Modulo",
+      "keywords": "api cargarcatalogo catalogo complemento controlador crudctrl datos del donde el en especificas extraer los method modulo moduloctrl object obtiene padre para sistema tareas url"
+    },
+    {
+      "section": "api",
+      "id": "Sistema.UsuarioCtrl",
+      "shortName": "Sistema.UsuarioCtrl",
+      "type": "object",
+      "moduleName": "Sistema",
+      "shortDescription": "Complemento del controlador CrudCtrl  para tareas especificas en Usuario",
+      "keywords": "$index addequipo addjurisdiccion agrega api autocomplete busqueda cambiar cambiartipo carga cargarcatalogo cat catalago catalogo checkbox clues clueschange cluesusuario cluesusuarioborrar codigo como complemento controlador crear crudctrl cuando datos de del denegados editar el elemento en equipo equipoborrar equipos especificas este evento existe ficha filtrado getclues getequipos getjurisdiccion grupo hacer identificador item juris jurisdicci jurisdiccion jurisdicciones la las limpia limpiarzona lista llenado los method modelos modo muestra nombre object objeto obtieene obtiene optiene para permisoeditgrupo permisogrupo permisogrupodelete permisos petici por posicion query querysearch quita se selecciona selecteditemchange si sistema tareas tipo una url usuario usuarioctrl valor zonas"
+    },
+    {
+      "section": "api",
+      "id": "Transaccion.CalidadCtrl",
+      "shortName": "Transaccion.CalidadCtrl",
+      "type": "object",
+      "moduleName": "Transaccion",
+      "shortDescription": "Complemento del controlador CrudCtrl  para tareas especificas en EvaluacionCalidad",
+      "keywords": "abre abrirficha acciones actual agrega agregarcolumna al ap api aprobar calidadctrl cambios carga cargar cargarcriterios cargarcriteriosver catalogo cerrar click clues clueschange codigo columna complemento comprobar comprueba cone controlador correspondan corresponden criterio criterios crudctrl datos de del dialog dialogo el en especificas estadistica estadisticas ev evaluaci evaluacion evaluacioncalidad evaluar evento exp expediente ficha getclues hide identificador indicador la las le los lugar method nivel numero object obtener obtenerpromedio obtiene optener otro para por promedio repita se si tareas tenga transaccion usuario valida validarnorepetirexpediente valor ver verificaci verificarcambios vista"
+    },
+    {
+      "section": "api",
+      "id": "Transaccion.HallazgoCtrl",
+      "shortName": "Transaccion.HallazgoCtrl",
+      "type": "object",
+      "moduleName": "Transaccion",
+      "shortDescription": "Complemento del controlador CrudCtrl  para tareas especificas en Hallazgo",
+      "keywords": "accion activos actual agrega agrupado al almacenar api aplicarfiltro array avanzado base bimestre buildtoggler calidad cambiaranio cambiarbimestre cambiarvertodoindicador cambiarvertodoum cargar categoria change click clues codigo complemento completa compprueba compsolo comrpueba con controlador correspondan crea criterio criterios crudctrl dato datorueba datos de del el en es especificas ev evaluaci evaluacion evaluaciones evento exista exists extraer fiiltro filtrado filtro firstclick getcluescriterios getcriterios getdimension hallazgo hallazgoctrl historico history identificador indicador indicadores informaci insertar item la las list lista listado los malos medicas method modelo mostrar navid nivel object objeto obtiene ocultar opciones origen para parametros pertenescan por posicion primer procesar quitar quitarfiltro recurso secondclick segundo si sidenav tareas tiene tipo toggle transaccion unidades valor ver verevaluacion verevaluacioncompleta"
+    },
+    {
+      "section": "api",
+      "id": "Transaccion.RecursoCtrl",
+      "shortName": "Transaccion.RecursoCtrl",
+      "type": "object",
+      "moduleName": "Transaccion",
+      "shortDescription": "Complemento del controlador CrudCtrl  para tareas especificas en EvaluacionRecurso",
+      "keywords": "abre abrirevaluacionficha abrirficha acciones actual al alguna ap api aprobar asignara callback cambios carga cargar cargarcatalogo cargarcriterios cargarcriteriosver catalogo cerrar click clues clueschange codigo complemento comprueba con cone controlador correspondan corresponden criterio criterios crudctrl cual cualquier datos de del despu dialog dialogo ejecutar el en especificas especifiquemos esta estadistica estadisticas ev evaluaci evaluacion evaluacionrecurso evaluar evento exitoso ficha funci funcion generar getclues hide identificador impresa indicador indicadores la las le listamos listar los lugar medica method modelo nivel nombre object obtener obtiene para petici por recurso recursoctrl resultado se seleccionar si tareas tenga transaccion una unidad url usuario ver verificaci verificarcambios vista"
+    }
+  ],
+  "apis": {
+    "api": true
+  },
+  "html5Mode": true,
+  "startPage": "/api/App.interface:Scaffolding",
+  "scripts": [
+    "angular.min.js"
+  ]
+};
